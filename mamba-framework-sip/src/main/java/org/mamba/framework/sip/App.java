270459d84823b1013c8a1ce6af39e61fdc83607f
八两俊 <lily_mcfly@sina.com>
2018-05-13 21:42:56 +0800

    新增sip模块

---
+++ mamba-framework-sip/src/main/java/org/mamba/framework/sip/App.java
@@ -0,0 +1,13 @@
+package org.mamba.framework.sip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
